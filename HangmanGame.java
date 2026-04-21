b4174694a587622b1509592234bdbc2109d2bad3
nwilber2026 <nwilber2026@cchsdons.com>
2026-04-21 09:20:39 -0700

    Add files via upload

---
+++ HangmanGame.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hangman.game;
+
+/**
+ *
+ * @author NWilber2026
+ */
+
+//------------------------------------------------------------------------------
+//Hangman Game Class
+public class HangmanGame {
+//
+//------------------------------------------------------------------------------
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+/*
+//Game Hub
+//
+//Start or View Completed Words
+//
+//If start:
+//Select Difficulty 
+    //(Ez=10lives)
+    //(MED=7lives)
+    //(HRD=5lives)
+//
+//Select theme
+    //Show all the words in the theme. 
+    //The ones that arent solved yet look like "_ _ _ _ _ _". 
+    //They then have to pick one of the unsolved words by number
+    //Theme ideas: superheros, 
+//
+//Load the word up for user (_ _ _ _ _ _ _)
+//
+//Allow user to guess a letter (or guess whole word)
+    //if doing points make more points for less total number of letter guesses
+//
+//Show Result. 
+    //If not in the word 
+        //Add a line to the hangman.
+        //Add letter to the guessed letters list
+    //If in the word
+        //Add the letters to the word
+//
+//Loop until game is over
+    //The hangman drawing is complete (--> Lose Screen and word reveal)
+    //The user guesses the word (--> Win Screen and add word to word bank)
+//
+Back to the start screen
+
+Start again
+*/
